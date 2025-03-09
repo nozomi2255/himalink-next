47f07c078aa6184804dea9a08ad6ffc46b72c1ba
Nozomi <nozomi.tomita.uhd@gmail.com>
2025-03-09 23:54:02 +0900

    CalendarViewにおけるany型の定義改善

--- components/CalendarView.tsx
+++ components/CalendarView.tsx
@@ -6,8 +6,17 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import timeGridPlugin from "@fullcalendar/timegrid";
 import interactionPlugin from "@fullcalendar/interaction";
 
+// Entryインターフェースの定義
+interface Entry {
+  id: string;
+  title: string;
+  start: string; // ISO 8601 format
+  end?: string; // ISO 8601 format
+  allDay?: boolean; // 終日イベントかどうか
+}
+
 interface CalendarViewProps {
-  events: any[];
+  events: Entry[]; // eventsの型をEntry[]に変更
   handleDateClick: (arg: { dateStr: string }) => void;
   handleEventClick: (arg: { event: { id: string } }) => void;
 }
