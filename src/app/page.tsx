3e6c0992bca5dc69a1805c8de6d58d2002529893
Nozomi <nozomi.tomita.uhd@gmail.com>
2025-03-29 10:33:20 +0900

    トップページにおけるヘッダーを廃止、サイドバー表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 // src/app/page.tsx
 import React from 'react';
-import CalendarHeader from "../components/CalendarHeader";
 import Sidebar from "../components/Sidebar";
 import CalendarView from "../components/CalendarView";
 import { getAuthenticatedUser } from '../app/actions';
@@ -19,21 +18,11 @@ export default async function CalendarPage() {
   const followers: UserRecord[] = await getFollowers(userRecord.id); // 実際のフォロワー情報を取得する処理に置き換える
 
   return (
-    <div className="relative min-h-screen p-4 bg-blue-50">
-      <div className="sticky top-0 left-0 right-0 z-50">
-      {/* ヘッダーに認証済みユーザーの詳細情報を渡す */}
-      <CalendarHeader 
-        userAvatarUrl={userRecord.avatar_url || "/path/to/default-avatar.png"} 
-        userName={userRecord.username || "ユーザー名"}  
-        showSearch={true}
-        followingUsers={followingUsers}
-        followers={followers}
-      />
-      </div>
+    <div className="relative min-h-screen p-0 bg-blue-50">
       {/* カレンダー表示 */}
-      <div className="mt-0 h-[calc(100vhpx)] flex">
+      <div className="mt-0 flex">
         <Sidebar />
-        <div className="flex-1 overflow-hidden">
+        <div className="flex-1">
           <CalendarView />
         </div>
       </div>
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,16 +1,32 @@
+'use client'
+
+import { useRouter } from "next/navigation";
+
 import React from "react";
 import "./Sidebar.css"; // スタイルは別ファイルで定義（後述）
 
 const Sidebar: React.FC = () => {
+  const router = useRouter();
+
+  const handleAvatarClick = () => {
+    router.push('/profile');
+  };
+
   return (
     <div className="sidebar">
       {/* プロフィールアイコン */}
-      <div className="avatar">N</div>
-
-      {/* ナビゲーションボタン */}
-      <div className="nav-section">
-        <button className="nav-button">👤 プロフィール</button>
-        <button className="nav-button">🔍 検索</button>
+      <div className="avatar" onClick={handleAvatarClick}>
+        <img
+          src="/path/to/avatar.png" // 必要に応じて動的なURLやユーザー情報を渡してください
+          alt="User Avatar"
+          style={{
+            width: "50px",
+            height: "50px",
+            borderRadius: "50%",
+            objectFit: "cover",
+            cursor: "pointer"
+          }}
+        />
       </div>
 
       {/* テンプレート予定 */}
