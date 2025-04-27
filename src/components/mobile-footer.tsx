eed51ea86a6c5c5429fa368188b12255cc6e5df6
Nozomi <nozomi.tomita.uhd@gmail.com>
2025-04-27 14:43:39 +0900

    イベントコンテナがfooterよりも高い位置に描画されないように調整

--- src/components/mobile-footer.tsx
+++ src/components/mobile-footer.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import { NavButtons } from "@/components/navi-items";
 
 const MobileFooter: React.FC = () => (
-  <footer className="fixed bottom-0 w-full flex md:hidden bg-white justify-center border-t p-2 z-20">
+  <footer className="fixed bottom-0 w-full flex md:hidden bg-white justify-center border-t p-2 z-[50]">
     <NavButtons />
   </footer>
 );
